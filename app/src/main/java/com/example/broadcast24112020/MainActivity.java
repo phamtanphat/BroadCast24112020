581a7d9ef02c446b318905ac1cf392c03b10e5b0
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-02-04 18:08:23 +0700

    revice broadcast

--- app/src/main/java/com/example/broadcast24112020/MainActivity.java
+++ app/src/main/java/com/example/broadcast24112020/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.broadcast24112020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyBroadCast mMyBroadCast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMyBroadCast = new MyBroadCast();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter("android.example.sendBroadCast");
+        registerReceiver(mMyBroadCast,filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mMyBroadCast);
     }
 }
